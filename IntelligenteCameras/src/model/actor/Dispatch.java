0050a07f2d0c09b2bf00a9e9838c88ff8b984432
MacCrimmon <ben.vandevorst@hotmail.com>
2017-11-06 21:34:35 +0100

    Added linkedlist Camera in Dispatch

--- IntelligenteCameras/src/model/actor/Dispatch.java
+++ IntelligenteCameras/src/model/actor/Dispatch.java
@@ -1,12 +1,15 @@
 package model.actor;
 
+import java.util.LinkedList;
+
+import model.Camera;
 import model.Locatie;
 
 public class Dispatch extends Actor{
-
+	private LinkedList<Camera> cameras;
+	
 	public Dispatch(Locatie locatie) {
 		super(locatie);
-		// TODO Auto-generated constructor stub
 	}
 
 }
