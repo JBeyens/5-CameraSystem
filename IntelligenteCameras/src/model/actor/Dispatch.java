51fcf250977c14ff565d851c245bb8fbea79fbc6
MacCrimmon <ben.vandevorst@hotmail.com>
2017-11-07 16:08:52 +0100

    Implemented methods and interfaces observer pattern

--- IntelligenteCameras/src/model/actor/Dispatch.java
+++ IntelligenteCameras/src/model/actor/Dispatch.java
@@ -1,15 +1,21 @@
 package model.actor;
 
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 import model.Camera;
 import model.Locatie;
+import observer.DispatchSubject;
+import observer.PatrouilleObserver;
 
-public class Dispatch extends Actor{
+public class Dispatch extends Actor implements DispatchSubject{
 	private LinkedList<Camera> cameras;
+	private Set<PatrouilleObserver> patrouilleObservers;
 	
 	public Dispatch(Locatie locatie) {
 		super(locatie);
+		patrouilleObservers = new HashSet<PatrouilleObserver>();
 	}
 	
 	
@@ -20,6 +26,27 @@ public class Dispatch extends Actor{
 	
 	public void addCamera(Camera camera){
 		this.cameras.add(camera);
+	}
+
+
+	@Override
+	public void doNotify() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addObserver(PatrouilleObserver observer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void removeObserver(PatrouilleObserver observer) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- IntelligenteCameras/src/model/actor/Patrouille.java
+++ IntelligenteCameras/src/model/actor/Patrouille.java
@@ -1,12 +1,19 @@
 package model.actor;
 
 import model.Locatie;
+import observer.PatrouilleObserver;
 
-public class Patrouille extends Actor{
+public class Patrouille extends Actor implements PatrouilleObserver{
 
 	public Patrouille(Locatie locatie) {
 		super(locatie);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doUpdate() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
