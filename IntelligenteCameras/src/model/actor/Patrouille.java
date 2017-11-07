8617cd14fee9305533fc3be9593bfd7cc51cb205
MacCrimmon <ben.vandevorst@hotmail.com>
2017-11-07 19:10:34 +0100

    PAtrouille comment added

--- IntelligenteCameras/src/model/actor/Patrouille.java
+++ IntelligenteCameras/src/model/actor/Patrouille.java
@@ -3,13 +3,27 @@ package model.actor;
 import model.Locatie;
 import observer.PatrouilleObserver;
 
+
+/**
+ * 	@Author Ben Vandevorst & Jef Beyens
+	@Datum 07/11/2017
+	@Project Intelligente Cameras
+	@Doel
+ */
+
 public class Patrouille extends Actor implements PatrouilleObserver{
 
+	/*
+	 * Constructor
+	 */
 	public Patrouille(Locatie locatie) {
 		super(locatie);
-		// TODO Auto-generated constructor stub
 	}
 
+	
+	/* (non-Javadoc)
+	 * @see observer.PatrouilleObserver#doUpdate()
+	 */
 	@Override
 	public void doUpdate() {
 		// TODO Auto-generated method stub
