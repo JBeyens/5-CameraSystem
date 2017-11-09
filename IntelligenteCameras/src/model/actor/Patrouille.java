804cc05e77d686aa0c232bb404561db4cab9c126
JBeyens <jefbeyens@gmail.com>
2017-11-09 14:15:26 +0100

    Observer pattern afgewerkt & tostrings bijgewerkt & comments toegevoegd

--- IntelligenteCameras/src/model/actor/Patrouille.java
+++ IntelligenteCameras/src/model/actor/Patrouille.java
@@ -1,10 +1,10 @@
 package model.actor;
 
 import java.util.HashSet;
-import java.util.Set;
 
 import model.Locatie;
 import observer.PatrouilleObserver;
+import utilities.StringOperations;
 
 
 /**
@@ -13,21 +13,32 @@ import observer.PatrouilleObserver;
 	@Project Intelligente Cameras
 	@Doel
  */
-
 public class Patrouille extends Actor implements PatrouilleObserver{
 	/** Fields **/
 	private static int patrouilleCounter = 0;
-	private Set<ITrackable> gesignalleerdTrackables;
+	private HashSet<ITrackable> gesignalleerdTrackables;
 
 	/** Constructor **/
 	public Patrouille(Locatie locatie) {
+    	// initialise instance variables
 		super(locatie);
-		setCounter(++patrouilleCounter);
 		gesignalleerdTrackables = new HashSet<ITrackable>();
+		// Verhogen van teller aantal objecten uit deze klasse & opslaan van nummer van dit object
+		setCounter(++patrouilleCounter); 
 	}
 
-	/** Methods **/
+	/** OBSERVER PATERN: Method waarop het subject deze observer kan verwittigen **/
 	public void ontvangGesignaleerdeTrackable(ITrackable gesignaleerdObject) {
 		gesignalleerdTrackables.add(gesignaleerdObject);
+	}
+	
+	/* Getter voor HashSet van gesignaleerde ITrackables */
+	public HashSet<ITrackable> getGesignaleerd() {
+		return gesignalleerdTrackables;
+	}
+	
+	/* Method die gesignaleerde objecten weergeeft als toString van elk element met de formatering in deze utility*/
+	public String getGesignaleerdString() {
+		return StringOperations.collectionToString(gesignalleerdTrackables);
 	}
 }
