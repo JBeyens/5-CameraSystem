28a76dd627399c6c8838899f731cbe1bd02ee1dd
JBeyens <jefbeyens@gmail.com>
2017-11-09 01:25:46 +0100

    Camera klasse hersteld -> String gefixt

--- IntelligenteCameras/src/model/actor/Camera.java
+++ IntelligenteCameras/src/model/actor/Camera.java
@@ -4,7 +4,8 @@ import factory.ActorFactory;
 import model.Foto;
 import model.Locatie;
 
-public class Camera extends Actor {
+public class Camera extends Actor
+{//triviale properties en methods van een Camera
     private static int count = 0;
     /**
      * Constructor for objects of class Camera
@@ -15,12 +16,10 @@ public class Camera extends Actor {
         super(locatie);
         setCounter(++count);
     }
-    
     public double getSnelheid(Voertuig voertuig){ 
     //snelheidsherkenning in km/u
         return 0.0;//dummy waarde
     }
-    
     public Voertuig getVoertuig(Foto foto){
     //herken voertuig op basis van foto (buiten scope)
         return (Voertuig)ActorFactory.createActor(ActorFactory.VOERTUIG, getLocatie());
@@ -30,10 +29,4 @@ public class Camera extends Actor {
         // fotograferen van een voertuig (buiten scope)
             return new Foto();
     }
-    
-    @Override
-	public String toString(){
-		return "\nCamera " + super.toString();
-	}
-
 }
