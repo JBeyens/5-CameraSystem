8c776f13122feea5c766e5698e95cb6ea9efd239
MacCrimmon <ben.vandevorst@hotmail.com>
2017-11-13 19:09:54 +0100

    Removed duplicated

--- IntelligenteCameras/src/model/actor/ITrackable.java
+++ IntelligenteCameras/src/model/actor/ITrackable.java
@@ -1,13 +1,10 @@
 package model.actor;
 
 public interface ITrackable extends ILocalisable{
-<<<<<<< HEAD
+
 
 	void setGeseind(Boolean isGeseind);
 	
 	Boolean getGeseind();
-=======
-	public void setGeseind(Boolean isGeseind);
-	public Boolean getGeseind();
->>>>>>> branch 'dev' of https://github.com/JBeyens/5-CameraSystem.git
+
 }
